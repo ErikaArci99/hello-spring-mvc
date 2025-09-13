5e626f22bd3bb88e01578c9d074d7162c99dc544
ErikaArci99 <erikaarci1999@outlook.it>
2025-09-13 17:51:25 +0200

    Add welcome endpoint and greeting template

--- src/main/java/org/lessons/java/spring/hello_spring_mvc/controllers/HomeController.java
+++ src/main/java/org/lessons/java/spring/hello_spring_mvc/controllers/HomeController.java
@@ -1,8 +1,12 @@
 package org.lessons.java.spring.hello_spring_mvc.controllers;
 
+import java.time.LocalDateTime;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping
@@ -11,5 +15,14 @@ public class HomeController {
     @GetMapping("/")
     public String home() {
         return "home";
+    }
+
+    @GetMapping("/welcome")
+    public String welcome(@RequestParam(name = "name") String name, Model model) {
+        model.addAttribute("email", "gina@gmail.com");
+        model.addAttribute("currentDate", LocalDateTime.now());
+        model.addAttribute("name", name);
+
+        return "greeting";
     }
 }
